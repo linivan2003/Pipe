0b616ff198bf4ae1462efbada434a628e8cc0bc7
Ivan Lin <linivan2003@g.ucla.edu>
2024-01-26 14:00:47 -0800

    error checking

--- pipe.c
+++ pipe.c
@@ -9,18 +9,24 @@ int main(int argc, char *argv[])
 	printf("number of command-line arguments: %d\n",argc);
 	//print each command line argument
 	for  (int i = 0; i<argc;i++){
-		printf("Argument %d: &s\n",i,argv[i]);
+		printf("Argument %d: %s\n",i,argv[i]);
 	}
 	int pipefd[2];
-	pipe(pipefd);
-	pid_t child_pid = fork();
+	if (pipe(pipefd) == -1){
+		perror("pipe");
+		return -1;
+	}
+	pid_t child_pid;
+	child_pid = fork();
+	if (child_pid == -1) {
+        perror("fork");
+        return -1;
+    }
 	if (child_pid == 0){
 		close(pipefd[0]);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
 		execlp(argv[1], argv[1], NULL);
-
-
 	} else{
 		close(pipefd[1]);
 		dup2(pipefd[0], STDIN_FILENO);
