d018845cc518273991b3ec9d5b9cfcc5bd3328b6
Ivan Lin <linivan2003@g.ucla.edu>
2024-01-25 16:11:32 -0800

    does one argument for command lines

--- pipe.c
+++ pipe.c
@@ -6,6 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
+	printf("number of command-line arguments: %d\n",argc);
+	//print each command line argument
+	for  (int i = 0; i<argc;i++){
+		printf("Argument %d: &s\n",i,argv[i]);
+	}
+	execlp(argv[1], argv[1], NULL);
+
+	printf("This line should not be reached.\n");
+
 	return 0;
 }
